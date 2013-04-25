d082c698c35a8987c5dc9cd7223c70a31bcf5fdf
Paul Harris <paulharris@computer.org>
2013-04-26 01:54:54 +0800

    Simplified statemachine

--- csvparser.hpp
+++ csvparser.hpp
@@ -56,61 +56,61 @@ struct Enewline {
   unsigned char value;
 };
 
-#define TTS(S,E,Snew,code) void on(States &self, S &s, const E &e) { code; self = Snew; }
+#define TTS(State, E, Snew, code) States on(State const&, E const& e) { code; return Snew(); }
 struct Trans {
   Trans(csv_builder &out) : error_message(NULL), out(out) {}
 
   const char* error_message;
   std::string whitespace_state;
 
-  //  State  Event        Next_State      Transition_Action
-  TTS(Start, Eqchar,      ReadQuoted(),   { out.begin_row(); });
-  TTS(Start, Esep,        ReadSkipPre(),  { out.begin_row(); next_cell(false); });
-  TTS(Start, Enewline,    self,           { out.begin_row(); out.end_row(); });
-  TTS(Start, Ewhitespace, ReadSkipPre(),  { out.begin_row(); });
-  TTS(Start, Echar,       ReadUnquoted(), { out.begin_row(); add(e); });
+  //  State          Event        Next_State    Transition_Action
+  TTS(Start,         Eqchar,      ReadQuoted,   { out.begin_row(); });
+  TTS(Start,         Esep,        ReadSkipPre,  { out.begin_row(); next_cell(false); });
+  TTS(Start,         Enewline,    Start,        { out.begin_row(); out.end_row(); });
+  TTS(Start,         Ewhitespace, ReadSkipPre,  { out.begin_row(); });
+  TTS(Start,         Echar,       ReadUnquoted, { out.begin_row(); add(e); });
 
-  TTS(ReadSkipPre, Eqchar,      ReadQuoted(),   {});
-  TTS(ReadSkipPre, Esep,        self,           { next_cell(false); });
-  TTS(ReadSkipPre, Enewline,    Start(),        { next_cell(false); out.end_row(); });
-  TTS(ReadSkipPre, Ewhitespace, self,           {});
-  TTS(ReadSkipPre, Echar,       ReadUnquoted(), { add(e); });
+  TTS(ReadSkipPre,   Eqchar,      ReadQuoted,   {});
+  TTS(ReadSkipPre,   Esep,        ReadSkipPre,  { next_cell(false); });
+  TTS(ReadSkipPre,   Enewline,    Start,        { next_cell(false); out.end_row(); });
+  TTS(ReadSkipPre,   Ewhitespace, ReadSkipPre,  {});
+  TTS(ReadSkipPre,   Echar,       ReadUnquoted, { add(e); });
 
-  TTS(ReadQuoted, Eqchar,      ReadQuotedCheckEscape(), {});
-  TTS(ReadQuoted, Esep,        self, { add(e); });
-  TTS(ReadQuoted, Enewline,    self, { add(e); });
-  TTS(ReadQuoted, Ewhitespace, self, { add(e); });
-  TTS(ReadQuoted, Echar,       self, { add(e); });
+  TTS(ReadQuoted,    Eqchar,      ReadQuotedCheckEscape, {});
+  TTS(ReadQuoted,    Esep,        ReadQuoted,            { add(e); });
+  TTS(ReadQuoted,    Enewline,    ReadQuoted,            { add(e); });
+  TTS(ReadQuoted,    Ewhitespace, ReadQuoted,            { add(e); });
+  TTS(ReadQuoted,    Echar,       ReadQuoted,            { add(e); });
 
-  TTS(ReadQuotedCheckEscape, Eqchar,      ReadQuoted(),  { add(e); });
-  TTS(ReadQuotedCheckEscape, Esep,        ReadSkipPre(), { next_cell(); });
-  TTS(ReadQuotedCheckEscape, Enewline,    Start(),       { next_cell(); out.end_row(); });
-  TTS(ReadQuotedCheckEscape, Ewhitespace, ReadQuotedSkipPost(), {});
-  TTS(ReadQuotedCheckEscape, Echar,       ReadError(),   { error_message = "char after possible endquote"; });
+  TTS(ReadQuotedCheckEscape, Eqchar,      ReadQuoted,          { add(e); });
+  TTS(ReadQuotedCheckEscape, Esep,        ReadSkipPre,         { next_cell(); });
+  TTS(ReadQuotedCheckEscape, Enewline,    Start,               { next_cell(); out.end_row(); });
+  TTS(ReadQuotedCheckEscape, Ewhitespace, ReadQuotedSkipPost,  {});
+  TTS(ReadQuotedCheckEscape, Echar,       ReadError,           { error_message = "char after possible endquote"; });
 
-  TTS(ReadQuotedSkipPost, Eqchar,      ReadError(),   { error_message = "quote after endquote"; });
-  TTS(ReadQuotedSkipPost, Esep,        ReadSkipPre(), { next_cell(); });
-  TTS(ReadQuotedSkipPost, Enewline,    Start(),       { next_cell(); out.end_row(); });
-  TTS(ReadQuotedSkipPost, Ewhitespace, self,          {});
-  TTS(ReadQuotedSkipPost, Echar,       ReadError(),   { error_message = "char after endquote"; });
+  TTS(ReadQuotedSkipPost, Eqchar,      ReadError,           { error_message = "quote after endquote"; });
+  TTS(ReadQuotedSkipPost, Esep,        ReadSkipPre,         { next_cell(); });
+  TTS(ReadQuotedSkipPost, Enewline,    Start,               { next_cell(); out.end_row(); });
+  TTS(ReadQuotedSkipPost, Ewhitespace, ReadQuotedSkipPost,  {});
+  TTS(ReadQuotedSkipPost, Echar,       ReadError,           { error_message = "char after endquote"; });
 
-  TTS(ReadUnquoted, Eqchar,      ReadError(),   { error_message = "unexpected quote in unquoted string"; });
-  TTS(ReadUnquoted, Esep,        ReadSkipPre(), { next_cell(); });
-  TTS(ReadUnquoted, Enewline,    Start(),       { next_cell(); out.end_row(); });
-  TTS(ReadUnquoted, Ewhitespace, ReadUnquotedWhitespace(), { whitespace_state.push_back(e.value); });
-  TTS(ReadUnquoted, Echar,       self,          { add(e); });
+  TTS(ReadUnquoted, Eqchar,      ReadError,              { error_message = "unexpected quote in unquoted string"; });
+  TTS(ReadUnquoted, Esep,        ReadSkipPre,            { next_cell(); });
+  TTS(ReadUnquoted, Enewline,    Start,                  { next_cell(); out.end_row(); });
+  TTS(ReadUnquoted, Ewhitespace, ReadUnquotedWhitespace, { whitespace_state.push_back(e.value); });
+  TTS(ReadUnquoted, Echar,       ReadUnquoted,           { add(e); });
 
-  TTS(ReadUnquotedWhitespace, Eqchar,      ReadError(),   { error_message = "unexpected quote after unquoted string"; });
-  TTS(ReadUnquotedWhitespace, Esep,        ReadSkipPre(),  { cell.append(whitespace_state); whitespace_state.clear(); next_cell(); });
-  TTS(ReadUnquotedWhitespace, Enewline,    Start(),        { cell.append(whitespace_state); whitespace_state.clear(); next_cell(); out.end_row(); });
-  TTS(ReadUnquotedWhitespace, Ewhitespace, self,           { whitespace_state.push_back(e.value); });
-  TTS(ReadUnquotedWhitespace, Echar,       ReadUnquoted(), { cell.append(whitespace_state); whitespace_state.clear(); add(e); });
+  TTS(ReadUnquotedWhitespace, Eqchar,      ReadError,                { error_message = "unexpected quote after unquoted string"; });
+  TTS(ReadUnquotedWhitespace, Esep,        ReadSkipPre,              { cell.append(whitespace_state); whitespace_state.clear(); next_cell(); });
+  TTS(ReadUnquotedWhitespace, Enewline,    Start,                    { cell.append(whitespace_state); whitespace_state.clear(); next_cell(); out.end_row(); });
+  TTS(ReadUnquotedWhitespace, Ewhitespace, ReadUnquotedWhitespace,   { whitespace_state.push_back(e.value); });
+  TTS(ReadUnquotedWhitespace, Echar,       ReadUnquoted,             { cell.append(whitespace_state); whitespace_state.clear(); add(e); });
 
-  TTS(ReadError, Eqchar, self, { assert(error_message); });
-  TTS(ReadError, Esep, self, { assert(error_message); });
-  TTS(ReadError, Enewline, self, { assert(error_message); });
-  TTS(ReadError, Ewhitespace, self, { assert(error_message); });
-  TTS(ReadError, Echar, self, { assert(error_message); });
+  TTS(ReadError, Eqchar, ReadError,       { assert(error_message); });
+  TTS(ReadError, Esep, ReadError,         { assert(error_message); });
+  TTS(ReadError, Enewline, ReadError,     { assert(error_message); });
+  TTS(ReadError, Ewhitespace, ReadError,  { assert(error_message); });
+  TTS(ReadError, Echar, ReadError,        { assert(error_message); });
 
 #undef TTS
 
@@ -132,41 +132,23 @@ private:
   std::string cell;
 };
 
-namespace detail {
-  template <class StateVariant,class Event,class Transitions>
-  struct NextState : boost::static_visitor<void> {
-    NextState(StateVariant &v,const Event &e,Transitions &t) : v(v),e(e),t(t) {}
+  template <class Event>
+  struct NextState : boost::static_visitor<States> {
+    NextState(const Event &e, Trans &t) : e(e),t(t) {}
 
     template <class State>
-    void operator()(State &s) const {
+    States operator()(State & s) const {
 #ifdef DEBUG
   printf("%s %c\n",typeid(State).name(),e.value);
 #endif
-      t.on(v,s,e);
+      return t.on(s,e);
     }
 
   private:
-    StateVariant &v;
     const Event &e;
-    Transitions &t;
+    Trans &t;
   };
-} // namespace detail
 
-#ifdef CPP11
-template <class Transitions=Trans,class StateVariant,class Event>
-void next(StateVariant &state,Event &&event,Transitions &&trans=Transitions()) {
-  boost::apply_visitor(detail::NextState<StateVariant,Event,Transitions>(state,std::forward<Event>(event),std::forward<Transitions>(trans)),state);
-}
-#else
-template <class Transitions,class StateVariant,class Event>
-void next(StateVariant &state,const Event &event,const Transitions &trans=Transitions()) {
-  boost::apply_visitor(detail::NextState<StateVariant,Event,Transitions>(state,event,trans),state);
-}
-template <class Transitions,class StateVariant,class Event>
-void next(StateVariant &state,const Event &event,Transitions &trans) {
-  boost::apply_visitor(detail::NextState<StateVariant,Event,Transitions>(state,event,trans),state);
-}
-#endif
 
 } // namespace csvFSM
 
@@ -189,15 +171,15 @@ bool operator()(const char *&buf,int len)
 {
   while (len > 0) {
     if (*buf == qchar) {
-      csvFSM::next(state,csvFSM::Eqchar(*buf),trans);
+      state = next(csvFSM::Eqchar(*buf));
     } else if (*buf == sep) {
-      csvFSM::next(state,csvFSM::Esep(*buf),trans);
+      state = next(csvFSM::Esep(*buf));
     } else if (*buf == ' ') { // TODO? more (but DO NOT collide with sep=='\t')
-      csvFSM::next(state,csvFSM::Ewhitespace(*buf),trans);
+      state = next(csvFSM::Ewhitespace(*buf));
     } else if (*buf == '\n') {
-      csvFSM::next(state,csvFSM::Enewline(*buf),trans);
+      state = next(csvFSM::Enewline(*buf));
     } else {
-      csvFSM::next(state,csvFSM::Echar(*buf),trans);
+      state = next(csvFSM::Echar(*buf));
     }
     if (trans.error_message) {
 #ifdef DEBUG
@@ -222,6 +204,11 @@ private:
 
   csvFSM::States state;
   csvFSM::Trans trans;
+
+   template <class Event>
+   csvFSM::States next(const Event & event) {
+     return boost::apply_visitor( csvFSM::NextState<Event>(event,trans), state);
+   }
 };
 
 
