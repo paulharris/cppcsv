333877d8ccdb592e388d9c3b9ff3a6c56bb103d1
Paul Harris <paulharris@computer.org>
2013-04-26 15:04:06 +0800

    Added my email to copyright message at the top.

--- csvparser.hpp
+++ csvparser.hpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2013 Tobias Hoffmann
-//               2013 Paul Harris
+//               2013 Paul Harris <paulharris@computer.org>
 //
 // License: http://opensource.org/licenses/MIT
 
